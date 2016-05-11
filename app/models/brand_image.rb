0cb8ad6707b2264081ad3b4222569dc29885da13
kmontag <kmontag@skillz.com>
2016-05-11 13:15:22 -0400

    missing annotations

--- app/models/brand_image.rb
+++ app/models/brand_image.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: brand_images
+#
+#  id            :integer          not null, primary key
+#  brand_id      :integer
+#  image_id      :integer
+#  match_quality :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class BrandImage < ActiveRecord::Base
   belongs_to :brand
   belongs_to :image
